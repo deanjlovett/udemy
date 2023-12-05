f234b20ec82ebb1fa8e2a4afdd45f0004abf31c9
Dean Lovett <dean.lovett@gmail.com>
2023-12-05 11:16:12 -0800

    adds range demo

--- zerotomastery.io-golang/src/lectures/demo/ranges/ranges.go
+++ zerotomastery.io-golang/src/lectures/demo/ranges/ranges.go
@@ -3,4 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	slice := []string {"Hello","world","!"}
+
+	// will not work because runes in string 
+	// are encoded using utf-8
+	//
+	// for i := 0; i<10; i++ {
+	// 	slice[0][i]
+	// }
+
+	for i, element := range slice {
+		fmt.Print(i, " ", element, " : ")
+		for _, ch := range element {
+			fmt.Printf(" %q", ch)
+		}
+		fmt.Println()
+	}
 }

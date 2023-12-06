1f96bc2de0d0da3deabc00f649bc783c2a17b9f2
Dean Lovett <dean.lovett@gmail.com>
2023-12-06 15:36:00 -0800

    adds demo reciever function

--- zerotomastery.io-golang/src/lectures/demo/rcv-func/rcv-func.go
+++ zerotomastery.io-golang/src/lectures/demo/rcv-func/rcv-func.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type Space struct {
+	occupied bool
+}
+
+type ParkingLot struct {
+	space []Space
+}
+
+func occupySpace(lot *ParkingLot, spaceNum int) {
+	
+}
+
 func main() {
 
 }

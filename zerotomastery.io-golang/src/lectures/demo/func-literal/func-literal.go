34b51c78c532e51da7e73dc46816f0aadf640a26
Dean Lovett <dean.lovett@gmail.com>
2023-12-06 15:38:27 -0800

    adds func literal exercise, work in progress

--- zerotomastery.io-golang/src/lectures/demo/func-literal/func-literal.go
+++ zerotomastery.io-golang/src/lectures/demo/func-literal/func-literal.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
-func main() {
+func add(lhs, rhs int) int {
+	return lhs + rhs
+}
 
+func compute(lhs, rhs int, op func(lhs, rhs int) int) int {
+	fmt.Printf("Running a computation with %v & %v\n", lhs, rhs)
+	return op(lhs, rhs)
+}
+
+
+func main() {
+	fmt.Println()
+
+	fmt.Println(" 2 + 2 =", compute(2,2,add))
+	fmt.Println()
+
+	fmt.Println("10 - 2 =", compute(
+		10,
+		2,
+		func(lhs,rhs int)int{
+			return lhs-rhs
+		},
+	))
+	fmt.Println()
+
+	mul := func(lhs, rhs int) int {
+		fmt.Printf("multiplying %v * %v = ", lhs, rhs)
+		return lhs * rhs
+	}
+	fmt.Println(compute(3,3,mul))
+	fmt.Println()
 }
--- zerotomastery.io-golang/src/lectures/exercise/func-literal/func-literal.go
+++ zerotomastery.io-golang/src/lectures/exercise/func-literal/func-literal.go
@@ -18,7 +18,12 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+	
 
 func main() {
 	lines := []string{
@@ -28,4 +33,75 @@ func main() {
 		"12 spaces,",
 		"and 4 punctuation marks in these lines of text!",
 	}
+	// generic function that takes a closure
+	ng := func(str string, fn func(r rune) bool) int {
+		count := 0
+		for _, char := range str{
+			if fn(char) {
+				count++
+			}
+		}
+		return count
+	}
+	//  - Number of letters	
+	nl := func(str string) int {
+		return ng(str,unicode.IsLetter)
+	}
+
+
+	//  - Number of digits
+	nd := func(str string) int {
+		return ng(str,unicode.IsDigit)
+	}
+	// nd := func(str string) int {
+	// 	count := 0
+	// 	for _, char := range str{
+	// 		if unicode.IsDigit(char) {
+	// 			count++
+	// 		}
+	// 	}
+	// 	return count
+	// }
+
+	//  - Number of spaces
+	ns := func(str string) int {
+		return ng(str,unicode.IsSpace)
+	}
+	// ns := func(str string) int {
+	// 	count := 0
+	// 	for _, char := range str{
+	// 		if unicode.IsSpace(char) .IsDigit(char) {
+	// 			count++
+	// 		}
+	// 	}
+	// 	return count
+	// }
+
+	//  - Number of punctuation marks
+	np := func(str string) int {
+		return ng(str,unicode.IsPunct)
+	}
+
+
+	fn := func(lns []string) {
+		catstr := strings.Join(lns,"")
+		//  - Number of letters
+		//  - Number of digits
+		//  - Number of spaces
+		//  - Number of punctuation marks
+		fmt.Println()
+		fmt.Println("          letters:", nl(catstr))
+		fmt.Println("           digits:", nd(catstr))
+		fmt.Println("           spaces:", ns(catstr))
+		fmt.Println("punctuation marks:", np(catstr))
+
+		fmt.Println()
+		fmt.Println("          letters:", ng(catstr,unicode.IsLetter))
+		fmt.Println("           digits:", ng(catstr,unicode.IsDigit))
+		fmt.Println("           spaces:", ng(catstr,unicode.IsSpace))
+		fmt.Println("punctuation marks:", ng(catstr,unicode.IsPunct))
+
+	}
+	fn(lines)
+
 }

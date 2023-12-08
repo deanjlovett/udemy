738b929092ff3d63f689d80a9ae2f8ac1cf1adb1
Dean Lovett <dean.lovett@gmail.com>
2023-12-07 21:24:27 -0800

    add demo & excercise for goroutines

--- zerotomastery.io-golang/src/lectures/exercise/goroutines/goroutines.go
+++ zerotomastery.io-golang/src/lectures/exercise/goroutines/goroutines.go
@@ -28,12 +28,84 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
+
+	// "io"
 	"os"
 	"strconv"
+	"sync"
 	"time"
+	// "math/rand"
 )
 
+func panicCheck(err error) {
+	if err != nil {
+		fmt.Println("Error:",err)
+		panic(err)
+	}
+}
+func check(err error) {
+	if err != nil {
+		fmt.Println("Error:",err)
+	}
+}
 func main() {
 	files := []string{"num1.txt", "num2.txt", "num3.txt", "num4.txt", "num5.txt"}
+
+	var wg sync.WaitGroup
+	
+
+	// cin := make(chan int,2*(len(files)+1))
+	cin := make(chan int, (len(files) + 1))
+	// make buffer large enough for all the file readers
+
+	fn := func(tag int, fname string, ci <-chan int, wg *sync.WaitGroup) {
+		wg.Add(1)
+		defer wg.Done()
+		sum := 0
+		f, err := os.Open(fname)
+		panicCheck(err)
+		defer f.Close()
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			sline := scanner.Text()
+			i, err := strconv.Atoi(sline)
+			if err == nil {
+				sum += i
+			}else{
+				fmt.Printf("Error file: %v, strlen: %v, string: \"%v\", msg{%v}\n", fname,len(sline),sline, err)
+			}
+		}
+		// added delay to test waiting for threads to start
+		//
+		// time.Sleep(time.Duration(rand.Intn(1000)+1) * time.Millisecond)
+
+		fmt.Println("File:",fname,"sum =", sum)
+		// cin<-tag
+		cin <- sum
+	}
+
+	for idx, fileName := range files {
+		fmt.Println("  inside for loop: idx:", idx, "fileName:", fileName)
+		go fn(idx+1, fileName, cin, &wg)
+	}
+
+	ncin := len(cin)
+	var waitCount time.Duration = 1
+	for ncin == 0 {
+		fmt.Println("  waiting for threads to write to channel. wait time:", waitCount)
+		time.Sleep(waitCount * time.Millisecond)
+		waitCount *= 2
+		ncin = len(cin)
+	}
+	wg.Wait()
+	sum := 0
+	for ncin > 0 {
+		// tag := <-cin
+		sum += <-cin
+		// fmt.Println("  inside for loop: reading from channel. tag:",tag)
+		fmt.Println("  inside for loop: reading from channel. # in buffer:", ncin)
+		ncin = len(cin)
+	}
+	close(cin)
+	fmt.Println("total sum =", sum)
 }

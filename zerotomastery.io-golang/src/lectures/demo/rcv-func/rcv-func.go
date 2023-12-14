82d8c1d7448009184bb8db2837a7bf457e341b77
Dean Lovett <dean.lovett@gmail.com>
2023-12-14 10:50:28 -0800

    add receiver funtion demo

--- zerotomastery.io-golang/src/lectures/demo/rcv-func/rcv-func.go
+++ zerotomastery.io-golang/src/lectures/demo/rcv-func/rcv-func.go
@@ -7,13 +7,34 @@ type Space struct {
 }
 
 type ParkingLot struct {
-	space []Space
+	spaces []Space
 }
 
 func occupySpace(lot *ParkingLot, spaceNum int) {
-	
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+// pointer receiver functions
+// 
+// almost like member functions or object methods
+
+// Same as above, different calling convention
+func (lot *ParkingLot) occupySpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkingLot) vacateSpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = false
 }
 
 func main() {
+	lot := ParkingLot{spaces: make([]Space, 4)}
+	fmt.Println("Initial:", lot)
 
+	lot.occupySpace(1)
+	occupySpace(&lot, 2)
+	fmt.Println("Occupied:", lot)
+
+	lot.vacateSpace(2)
+	fmt.Println("After vacate:", lot)
 }

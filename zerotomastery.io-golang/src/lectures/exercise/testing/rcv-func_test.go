1a6b5bffd13ba43f70cdfe3617dc28d7b53a2b86
Dean Lovett <dean.lovett@gmail.com>
2023-12-14 19:22:55 -0800

    adds testing exercise

--- zerotomastery.io-golang/src/lectures/exercise/testing/rcv-func_test.go
+++ zerotomastery.io-golang/src/lectures/exercise/testing/rcv-func_test.go
@@ -10,3 +10,138 @@
 //
 //--Notes:
 //* Use `go test -v ./exercise/testing` to run these specific tests
+
+package main
+import "testing"
+// import "fmt"
+
+func TestAbove(t *testing.T){
+
+	tp := NewPlayer("test_player",100,1000)
+	if tp.health != 0 {
+		t.Errorf("incorrect health: %v, want %v", tp.health, 0)
+	}else{
+		tp.SetHealth(tp.maxHealth)
+		if tp.health != tp.maxHealth {
+			t.Errorf("incorrect health: %v, want %v", tp.health, tp.maxHealth)
+		}
+		tp.ModHealth(int(tp.maxHealth) + 1)
+		if tp.health != tp.maxHealth {
+			t.Errorf("incorrect health: %v, want %v", tp.health, tp.maxHealth)
+		}
+	}
+	if tp.energy != 0 {
+		t.Errorf("incorrect energy: %v, want %v", tp.energy, 0)
+	}else{
+		tp.SetEnergy(tp.maxEnergy)
+		if tp.energy != tp.maxEnergy {
+			t.Errorf("incorrect energy: %v, want %v", tp.energy, tp.maxEnergy)
+		}
+		tp.ModEnergy(int(tp.maxEnergy) + 1)
+		if tp.energy != tp.maxEnergy {
+			t.Errorf("incorrect energy: %v, want %v", tp.energy, tp.maxEnergy)
+		}
+	}
+}
+
+func TestBelow(t *testing.T){
+
+	tp := NewPlayer("test_player",100,1000)
+	if tp.health != 0 {
+		t.Errorf("incorrect health: %v, want %v", tp.health, 0)
+	}else{
+		tp.SetHealth(0)
+		if tp.health != 0 {
+			t.Errorf("incorrect health: %v, want %v", tp.health, 0)
+		}
+		tp.ModHealth(-1)
+		if tp.health != 0 {
+			t.Errorf("incorrect health: %v, want %v", tp.health, 0)
+		}
+	}
+	if tp.energy != 0 {
+		t.Errorf("incorrect energy: %v, want %v", tp.energy, 0)
+	}else{
+		tp.SetEnergy(0)
+		if tp.energy != 0 {
+			t.Errorf("incorrect energy: %v, want %v", tp.energy, 0)
+		}
+		tp.ModEnergy(-1)
+		if tp.energy != 0 {
+			t.Errorf("incorrect energy: %v, want %v", tp.energy, 0)
+		}
+	}
+}
+
+func TestEnergy(t *testing.T){
+	// test min
+	// fmt.Println("testing for excedding min energy")
+
+	tp := NewPlayer("test_player",100,1000)
+	if tp.energy != 0 {
+		t.Errorf("incorrect energy: %v, want %v", tp.energy, 0)
+	}else{
+		tp.SetEnergy(0)
+		if tp.energy != 0 {
+			t.Errorf("incorrect energy: %v, want %v", tp.energy, 0)
+		}
+		tp.ModEnergy(-1)
+		if tp.energy != 0 {
+			t.Errorf("incorrect energy: %v, want %v", tp.energy, 0)
+		}
+	}
+
+	// test max
+	// fmt.Println("testing for excedding max energy")
+	tp = NewPlayer("test_player",100,1000)
+
+	if tp.energy != 0 {
+		t.Errorf("incorrect energy: %v, want %v", tp.energy, 0)
+	}else{
+		tp.SetEnergy(tp.maxEnergy)
+		if tp.energy != tp.maxEnergy {
+			t.Errorf("incorrect energy: %v, want %v", tp.energy, tp.maxEnergy)
+		}
+		tp.ModEnergy(int(tp.maxEnergy) + 1)
+		if tp.energy != tp.maxEnergy {
+			t.Errorf("incorrect energy: %v, want %v", tp.energy, tp.maxEnergy)
+		}
+	}
+
+}
+func TestHealth(t *testing.T){
+	// test min
+	// fmt.Println("test min health")
+
+	tp := NewPlayer("test_player",100,1000)
+	if tp.health != 0 {
+		t.Errorf("incorrect health: %v, want %v", tp.health, 0)
+	}else{
+		tp.SetHealth(0)
+		if tp.health != 0 {
+			t.Errorf("incorrect health: %v, want %v", tp.health, 0)
+		}
+		tp.ModHealth(-1)
+		if tp.health != 0 {
+			t.Errorf("incorrect health: %v, want %v", tp.health, 0)
+		}
+	}
+
+	// test max
+	// fmt.Println("test max health")
+
+	tp = NewPlayer("test_player",100,1000)
+	if tp.health != 0 {
+		t.Errorf("incorrect health: %v, want %v", tp.health, 0)
+	}else{
+		tp.SetHealth(tp.maxHealth)
+		if tp.health != tp.maxHealth {
+			t.Errorf("incorrect health: %v, want %v", tp.health, tp.maxHealth)
+		}
+		tp.ModHealth(int(tp.maxHealth) + 1)
+		if tp.health != tp.maxHealth {
+			t.Errorf("incorrect health: %v, want %v", tp.health, tp.maxHealth)
+		}
+	}
+	
+}

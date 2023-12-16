369aa003e902cb48ea1024a9a63a296d1854aaf6
Dean Lovett <dean.lovett@gmail.com>
2023-12-16 11:28:09 -0800

    revised excercise errors based on provided solution

--- zerotomastery.io-golang/src/lectures/exercise/errors/errors.go
+++ zerotomastery.io-golang/src/lectures/exercise/errors/errors.go
@@ -14,11 +14,11 @@
 //* Use the `strconv` package from stdlib to convert strings to ints
 //* Use the `errors` package to generate errors
 
-//package timeparse
-package main
+package timeparse
+
+//package main
 
 import (
-	// "errors"
 	"fmt"
 	"strconv"
 	"strings"
@@ -28,73 +28,62 @@ import (
 //  - Hour, minute, and second integer components
 
 type MyTime struct {
-	hours uint
-	minutes uint
-	seconds uint
+	hours, minutes, seconds int
 }
 
 func (mt *MyTime) toString() string {
-	return fmt.Sprintf("%2d:%02d:%02d",mt.hours, mt.minutes, mt.seconds)
+	return fmt.Sprintf("%2d:%02d:%02d", mt.hours, mt.minutes, mt.seconds)
 }
 
-func timeParse(in string) (MyTime, error){
-	myTimeErr := MyTime{0,0,0}
-	myTimeRet := MyTime{0,0,0}
-	count := strings.Count(in,":")
-	if count > 2 {
+type MyTimeParseError struct {
+	str   string
+	input string
+}
+
+func (t *MyTimeParseError) Error() string {
+	return fmt.Sprintf("%v: %v", t.str, t.input)
+}
+
+func TimeParse(input string) (MyTime, error) {
+
+	myTimeErr := MyTime{0, 0, 0}
+
+	myArr := strings.Split(input, ":")
+	count := len(myArr)
+	if count < 2 || count > 3 {
 		// return myTimeErr, errors.New(fmt.Sprintf("invalid time formate: too many colons"))
-		return myTimeErr, fmt.Errorf("invalid time formate: too many colons")
+		// return myTimeErr, fmt.Errorf("invalid time formate: too many colons")
+		return myTimeErr, &MyTimeParseError{"Invalid number of time components", input}
 	}
-	myArr := strings.Split(in,":")
-	h, err := strconv.Atoi(myArr[0])
+
+	//* If parsing fails, then a descriptive error must be returned
+
+	hours, err := strconv.Atoi(myArr[0])
 	if err != nil {
-		return myTimeErr, err
-	}else if h<0 || h>23 {
-		// return myTimeErr,errors.New(fmt.Sprintf("invalid time format: the hours must bewteen 0 and 23."))
-		return myTimeErr,fmt.Errorf("invalid time format: the hours must bewteen 0 and 23.")
+		return myTimeErr, &MyTimeParseError{fmt.Sprintf("Error parsing hours: %v", err), input}
 	}
-	myTimeRet.hours = uint(h)
-	if len(myArr) > 1 {
-		m, err := strconv.Atoi(myArr[1])
+
+	minutes, err := strconv.Atoi(myArr[1])
+	if err != nil {
+		return myTimeErr, &MyTimeParseError{fmt.Sprintf("Error parsing minutes: %v", err), input}
+	}
+
+	seconds := 0
+	if count == 3 {
+		seconds, err = strconv.Atoi(myArr[2])
 		if err != nil {
-			// return myTimeErr, err
-			// return myTimeErr, errors.New(fmt.Sprintf("invalid time format: can't parse minutes characters"))
-			return myTimeErr, fmt.Errorf("invalid time format: can't parse minutes characters")
-		}else if m<0 || m>59  || (m<10 && len(myArr[1])==1){
-			// return myTimeErr,errors.New(fmt.Sprintf("invalid time format: the minutes must bewteen 0 and 59."))
-			return myTimeErr, fmt.Errorf("invalid time format: the minutes must bewteen 0 and 59.")
-		}	
-		myTimeRet.minutes = uint(m)
+			return myTimeErr, &MyTimeParseError{fmt.Sprintf("Error parsing seconds: %v", err), input}
+		}
 	}
-	if len(myArr) == 3 {
-		sec, err := strconv.Atoi(myArr[2])
-		if err != nil {
-			// return myTimeErr, err
-			// return myTimeErr, errors.New(fmt.Sprintf("invalid time format: can't parse seconds characters"))
-			return myTimeErr, fmt.Errorf("invalid time format: can't parse seconds characters")
-		}else if sec<0 || sec>59 || (sec<10 && len(myArr[2])==1){
-			// return myTimeErr, errors.New(fmt.Sprintf("invalid time format: the seconds must bewteen 0 and 59."))
-			return myTimeErr, fmt.Errorf("invalid time format: the seconds must bewteen 0 and 59.")
-		}	
-		myTimeRet.seconds = uint(sec)
+
+	if hours < 0 || hours > 23 {
+		return myTimeErr, &MyTimeParseError{"hours out of range: 0 <= hour <= 23", fmt.Sprintf("%v", hours)}
 	}
-	return myTimeRet,nil
+	if minutes > 59 || minutes < 0 {
+		return myTimeErr, &MyTimeParseError{"minutes out of range: 0 <= minute <= 59", fmt.Sprintf("%v", minutes)}
+	}
+	if seconds > 59 || seconds < 0 {
+		return myTimeErr, &MyTimeParseError{"seconds out of range: 0 <= second <= 59", fmt.Sprintf("%v", seconds)}
+	}
+	return MyTime{hours, minutes, seconds}, nil
 }
-
-func main() {
-	testArr :=[]string{
-		"0:0:0",
-		"0:00:01",
-		"1:20:30",
-		"25:20:30",
-		"1:60:30",
-		"1:20:60",
-		"-1:20:59",
-	}
-	for i,testTime := range testArr {
-		myTime, err := timeParse(testTime)
-		fmt.Printf("%v: %10s, %10s, error:%v\n",i,testTime,myTime.toString(),err)
-
-	}
-}
-

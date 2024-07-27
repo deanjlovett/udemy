55705d6453463fefc71db511e37369cff5cebcf1
Dean Lovett <dean.lovett@gmail.com>
2024-07-26 20:33:14 -0700

    renamed section-2-assignment-2 qt6cb-02-assignment-2

--- qt-6-core-beginners/section-2-assignment-2/blank-qt-console-app/build/Qt_6_7_2_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_blank-qt-console-app_FILE /Users/dean/Desktop/sdcs/github-projects/udemy/qt-6-core-beginners/section-2-assignment-2/blank-qt-console-app/build/Qt_6_7_2_for_macOS-Debug/blank-qt-console-app)
-set(__QT_DEPLOY_TARGET_blank-qt-console-app_TYPE EXECUTABLE)

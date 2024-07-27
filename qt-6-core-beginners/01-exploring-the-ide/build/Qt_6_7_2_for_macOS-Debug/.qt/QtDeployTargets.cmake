600b6647545ba67ec37db61a92d70899f8728fb9
Dean Lovett <dean.lovett@gmail.com>
2024-07-26 20:29:57 -0700

    renamed 01-exploring-the-ide qt6cb-01-exploring-the-ide

--- qt-6-core-beginners/01-exploring-the-ide/build/Qt_6_7_2_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_01-exploring-the-ide_FILE /Users/dean/Desktop/sdcs/github-projects/udemy/qt-6-core-beginners/01-exploring-the-ide/build/Qt_6_7_2_for_macOS-Debug/01-exploring-the-ide)
-set(__QT_DEPLOY_TARGET_01-exploring-the-ide_TYPE EXECUTABLE)
